34ec3bf10e80b3e97539f6e518865951fd4f9114
Lemore Sebastien <11504471@r10115.iutv.univ-paris13.fr>
2016-11-18 09:33:33 +0100

    first commit

---
+++ SortAttaque.java
@@ -0,0 +1,13 @@
+
+public class SortAttaque extends Sort {
+	
+	public SortAttaque(String nom, int PUI, int FAC) {
+		super(nom,PUI,FAC);
+	}
+	
+	public SortAttaque(SortAttaque sortatt){
+		super(sortatt);
+	}
+
+}
+# Java-RPG
